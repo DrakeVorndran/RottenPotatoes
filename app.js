520b8faff3964fe2fed8a042401addd6231ae5d2
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-10-23 14:48:36 -0700

    users can see single reviews

--- app.js
+++ app.js
@@ -30,24 +30,36 @@ app.get("/", (req,res)=>{
         .then(reviews =>{
     res.render('reviews-index', {reviews: reviews});
         })
-    .catch(err=>{
+    .catch( err => {
         console.log(err);
     })
 });
+
 
 
 app.get('/reviews/new', (req,res) =>{
     res.render('reviews-new',{});
 });
 
+//create
+
 app.post("/reviews", (req,res) =>{
     Review.create(req.body).then((review) => {
         console.log(review);
-        res.redirect('/');
+        res.redirect('/reviews/${review._id}');
+    }).catch((err) => {
+        console.log(err.message);
+    });
+});
+
+
+app.get("/reviews/:id",(req,res)=>{
+    Review.findById(req.params.id).then((review) => {
+        res.render('reviews-show', {review: review});
     }).catch((err) => {
         console.log(err.message);
     })
-})
+});
 
 
 app.listen(3000, () => {
