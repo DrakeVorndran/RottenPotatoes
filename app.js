8aadb486bc48fb5d819f7da84f7b882804b1708f
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-10-28 15:59:28 -0700

    trying to get heroku working

--- app.js
+++ app.js
@@ -31,9 +31,11 @@ const reviews = require('./controllers/reviews')(app);
 
 
 
-
 const port = process.env.PORT || 3000;
 app.listen(port);
+app.listen(port, () => {
+    console.log("app listening on port 3000");
+});
 
 
 
