a7612ef1d7b022466bce381ea678ab5e07bccf2f
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-10-30 11:11:59 -0700

    comments can be deleted

--- controllers/comments.js
+++ controllers/comments.js
@@ -9,4 +9,13 @@ module.exports = (app) => {
             console.log(err.message);
         });
     });
+    
+    app.delete('/reviews/comments/:id', (req, res) => {
+        console.log("DELETE comment")
+        Comment.findByIdAndRemove(req.params.id).then((comment) => {
+            res.redirect(`/reviews/${comment.reviewId}`);
+        }).catch((err) => {
+            console.log(err.message)
+        })
+    })
 }
